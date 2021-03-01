1eb72bc149c26cb102cea02de39f8611d89ddd34
Arjuna Rawat <rawat4arjuna@gmail.com>
2021-03-01 19:36:19 +0530

    done

--- src/Context/AppContext.js
+++ src/Context/AppContext.js
@@ -50,17 +50,12 @@ let reducer = (state, action) => {
 };
 
 function ContextOneProvider(props) {
-  // [A]
   let [state, dispatch] = React.useReducer(reducer, initialState);
   let value = { state, dispatch };
-
-  // [B]
   return (
     <ContextOne.Provider value={value}>{props.children}</ContextOne.Provider>
   );
 }
 
 let ContextOneConsumer = ContextOne.Consumer;
-
-// [C]
 export { ContextOne, ContextOneProvider, ContextOneConsumer };
